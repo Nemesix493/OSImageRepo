3920a35b17d31823c170a0cd11c7cf65a670c427
Nemesix493 <verquerre.kevin@gmail.com>
2025-03-04 12:52:19 +0100

    feat: add GET endpoints
    Add GET endpoints to simplify the Nginx configuration
    and facilitate the setup of custom indexes in the future.

--- api.py
+++ api.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import re
 from os import makedirs, remove
 
-from flask import Flask, request, jsonify, current_app
+from flask import Flask, request, jsonify, current_app, Response
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
@@ -20,14 +20,15 @@ def is_in_upload_path(resolved_path: Path) -> bool:
     return resolved_path.is_relative_to(current_app.config['UPLOAD_PATH'])
 
 
-@app.route("/<path:dir_path>", methods=['POST', 'PATCH'])
+@app.route("/<path:dir_path>", methods=['GET', 'POST', 'PATCH'])
 def upload(dir_path):
     resolved_dir_path = current_app.config['UPLOAD_PATH'] / dir_path
     if not (is_safe_path(dir_path) and is_in_upload_path(resolved_dir_path)):
         return jsonify({'error_message': 'not valid path'}), 400
     func_method = {
         'POST': upload_post,
-        'PATCH': upload_patch
+        'PATCH': upload_patch,
+        'GET': upload_get
     }
     return func_method[request.method](resolved_dir_path)
 
@@ -50,3 +51,22 @@ def upload_patch(resolved_dir_path: Path):
             remove(file_path)
         file.save(file_path)
     return jsonify({'message': 'Successfully updated'}), 200
+
+
+def upload_get(resolved_dir_path: Path):
+    file_path = resolved_dir_path.relative_to(current_app.config['UPLOAD_PATH'])
+    response = Response(status=200)
+    if resolved_dir_path.is_dir():
+        response.headers['X-Accel-Redirect'] = f'/files/{file_path}/'
+    else:
+        response.headers['X-Accel-Redirect'] = f'/files/{file_path}'
+    del response.headers['Content-Type']
+    return response
+
+
+@app.route('/')
+def root():
+    response = Response(status=200)
+    response.headers['X-Accel-Redirect'] = '/files/'
+    del response.headers['Content-Type']
+    return response
